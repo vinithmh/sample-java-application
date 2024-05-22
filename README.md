8cf086be277d3b56ad4564789dc37d2bfcf85d96
Ubuntu <ubuntu@ip-172-31-16-19.ec2.internal>
2024-05-22 02:46:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Java App
+
+This is a simple Spring Boot application.
+
+## Endpoints
+
+- `/hello` - Returns "Hello, World!"
---
+++ src/main/java/com/example/MyJavaApp/MyJavaAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.MyJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJavaAppApplication.class, args);
+    }
+}
